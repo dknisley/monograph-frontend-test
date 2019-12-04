a50b7b54ffde7b5863f4901dde3759b7e6aec22a
Dan Knisley <dan@form.co>
2019-12-04 11:57:28 -0600

    Remove unnecessary imported components from App.js
    These components were moved to the Review component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import './App.css'
 import ReviewProvider from './state/ReviewProvider'
-import Header from './components/header/Header'
-import Form from './components/form/Form'
 import Review from './Review'
 import Thanks from './components/thanks/Thanks'
 
